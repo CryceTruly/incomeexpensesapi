bad95f83e6fc7ffe09d054163a72c55996308219
crycetruly <crycetruly@gmail.com>
2020-06-02 01:07:06 +0200

    Remove unnecesary spaces

--- authentication/views.py
+++ authentication/views.py
@@ -32,13 +32,12 @@ class RegisterView(generics.GenericAPIView):
         serializer.save()
         user_data = serializer.data
         user = User.objects.get(email=user_data['email'])
-
         token = RefreshToken.for_user(user).access_token
-
         current_site = get_current_site(request).domain
         relativeLink = reverse('email-verify')
         absurl = 'http://'+current_site+relativeLink+"?token="+str(token)
-        email_body = 'Hi '+user.username+' Use link below to verify your email \n' + absurl
+        email_body = 'Hi '+user.username + \
+            ' Use the link below to verify your email \n' + absurl
         data = {'email_body': email_body, 'to_email': user.email,
                 'email_subject': 'Verify your email'}
 
