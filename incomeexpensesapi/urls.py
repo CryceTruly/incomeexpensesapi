5b386fb4874a4b8c220cd11d3f6343bc7cbd8f21
crycetruly <crycetruly@gmail.com>
2020-09-17 12:02:33 +0200

    ch(google): Add google Login

--- incomeexpensesapi/urls.py
+++ incomeexpensesapi/urls.py
@@ -37,6 +37,9 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
+
+    path('social_auth/', include(('social_auth.urls', 'social_auth'),
+                                 namespace="social_auth")),
     path('expenses/', include('expenses.urls')),
     path('income/', include('income.urls')),
     path('', schema_view.with_ui('swagger',
