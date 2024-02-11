9af6d986e41fc924c5a13a60d8b50195f30c862a
MESA.D <samrids@gmail.com>
2024-02-11 12:26:58 +0700

    Support Django==3.2.20

--- incomeexpensesapi/settings.py
+++ incomeexpensesapi/settings.py
@@ -168,3 +168,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
